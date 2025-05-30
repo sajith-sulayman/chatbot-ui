afb89da3fec839721e2a3aeb4a93cc2005cbc236
sajith-sulayman <sajith.suliman@gmail.com>
2025-05-30 21:59:59 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,27 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
+  enabled: process.env.ANALYZE === "true",
+});
 
 const withPWA = require("next-pwa")({
-  dest: "public"
-})
+  dest: "public",
+});
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  // i18n removed for flat routing
+  images: {
+    remotePatterns: [
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "http", hostname: "127.0.0.1" },
+      { protocol: "https", hostname: "**" },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+  },
+};
+
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
