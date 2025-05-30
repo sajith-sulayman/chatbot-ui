d1b5dcfa8ce681223857c54f6c3cdc799dae3b84
sajith-sulayman <sajith.suliman@gmail.com>
2025-05-30 21:52:10 +0400

    Delete app/[locale] directory

--- app/[locale]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-// disable static prerender for this route
-export const dynamic = "force-dynamic";
-
-export default function ChatLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
